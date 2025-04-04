8b0d154eae17e3a4f3135d8645ffa51ac6b0d625
ShelarAtharva404 <23amtics404@gmail.com>
2025-04-04 22:50:15 +0530

    Update and rename day4.js to day4.html

--- day4.js
+++
@@ -1,8 +0,0 @@
-function filterEvenNumbers(arr) {
-    return arr.filter(num => num % 2 === 0);
-}
-
-// Example usage
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,22,34,55,3,21,23,44,55,67,78,90];
-const evenNumbers = filterEvenNumbers(numbers);
-console.log(evenNumbers);
